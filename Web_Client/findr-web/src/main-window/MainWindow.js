a54b9e256793aab39d6300c0e642b99fc3a3c1d1
David D <david.dinkevich@gmail.com>
2023-03-23 17:26:34 +0200

    Added ability to choose a different video once uploaded

--- Web_Client/findr-web/src/main-window/MainWindow.js
+++ Web_Client/findr-web/src/main-window/MainWindow.js
@@ -2,13 +2,13 @@
 
 
 //     return (<>
-        
-    
+
+
 //     </>);
 // }
 
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Dropzone from 'react-dropzone';
 import ReactPlayer from 'react-player';
 import './main-window.css';
@@ -25,23 +25,35 @@ function VideoUploader() {
     }
   };
 
+  const button_uploadNewVideo = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'video/*,image/*';
+    input.onchange = (event) => {
+      setVideoFile(event.target.files[0]);
+    };
+    input.click();
+  }
+
   return (
     <div id='main_div'>
       <label id='logo'>Findr</label>
-      <Dropzone onDrop={handleDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <div id='video-drop-zone' {...getRootProps()} style={{ border: 'dashed 2px gray', display: 'flex', justifyContent: 'center', width: '80%', height: '200px' }}>
-            <input {...getInputProps()} accept="video/*" />
-            <p style={{margin: 'auto'}}>Drag and drop a video or image file here.</p>
+      {!videoFile ? (
+        <Dropzone onDrop={handleDrop}>
+          {({ getRootProps, getInputProps }) => (
+            <div id='video-drop-zone' {...getRootProps()} style={{ border: 'dashed 2px gray', display: 'flex', justifyContent: 'center', width: '80%', height: '200px' }}>
+              <input {...getInputProps()} accept="video/*" />
+              <p style={{ margin: 'auto' }}>Drag and drop a video or image file here.</p>
+            </div>
+          )}
+        </Dropzone>)
+        : <div>
+            <ReactPlayer url={URL.createObjectURL(videoFile)} style={{margin:'auto'}} controls width="80%" height="400px" />
+            <button type="button" className="btn btn-light main-window-button" onClick={button_uploadNewVideo}>Choose a different video</button>
           </div>
-        )}
-      </Dropzone>
-      {videoFile && (
-        <div>
-          <ReactPlayer url={URL.createObjectURL(videoFile)} controls width="100%" height="auto" />
-        </div>
-      )}
-      <input id='query_bar' type="text" class="form-control" placeholder="Enter your query here"/>
+      }
+
+      <input id='query_bar' type="text" className="form-control" placeholder="Enter your query here" />
 
     </div>
   );

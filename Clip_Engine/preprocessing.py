2d8f0670a744f2899d63305fb6561c2666420dc8
David D <david.dinkevich@gmail.com>
2023-04-17 16:38:58 +0300

    Added frame-skipping optimization and consolidated functions

--- Clip_Engine/preprocessing.py
+++ Clip_Engine/preprocessing.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def get_significant_frames(filename):
+def _get_significant_frames(filename, similarity_threshold, skip_rate):
     cap = cv2.VideoCapture(filename)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -13,13 +13,17 @@ def get_significant_frames(filename):
 
     frame_index = 0
     curr_model_frame = None
-    num_kept = 0
     while True:
-        ret, frame = cap.read()
-        print(f"Frame number {frame_index}/{total_frames}")
-
+        ret = cap.grab()
         if not ret: # haven't reached the end yet
             break
+        # Skip frames
+        if frame_index % skip_rate != 0:
+            frame_index += 1
+            continue
+
+        _, frame = cap.retrieve() # Decode
+        print(f"Frame number {frame_index}/{total_frames}")
 
         if frame_index == 0: # First frame
             # Store original video shape
@@ -36,25 +40,19 @@ def get_significant_frames(filename):
             # Compute cosine similarity between the two frames
             sim = np.dot(curr_model_frame, processed_frame) / (np.linalg.norm(curr_model_frame) * np.linalg.norm(processed_frame))
             print(f'Similarity is: {sim}')
-            if sim >= 0.9:
-                print('dropped')
-            else:
-                print('kept -- NEW MODEL')
-
-
-                num_kept += 1
+            if sim < similarity_threshold:
                 # Update model frame: first convert frame to grayscale, then flatten
                 curr_model_frame = frame.flatten().astype('float')
                 significant_frames.append(frame)
 
         frame_index += 1
 
-    print(f'Num kept: {num_kept}')
+    print(f'Num kept: {len(significant_frames)}')
     cap.release() # Release resources
     return significant_frames, fps, width, height
 
 
-def create_compressed_video(significant_frames, output_filename, fps, width, height):
+def _create_compressed_video(significant_frames, output_filename, fps, width, height):
     # Define output video properties
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     out = cv2.VideoWriter(output_filename, fourcc, fps, (width, height))
@@ -65,7 +63,11 @@ def create_compressed_video(significant_frames, output_filename, fps, width, hei
     out.release()
 
 
-if __name__ == '__main__':
-    sig_frames, fps, w, h = get_significant_frames('giraffe_and_hippo.mp4')
-    create_compressed_video(sig_frames, 'compressed_video.mp4', fps, w, h)
+def compress_video(input_file, output_file, similarity_threshold=0.9, skip_rate=3):
+    import time
+    sig_frames, fps, w, h = _get_significant_frames(input_file, similarity_threshold=0.9, skip_rate=3)
+    _create_compressed_video(sig_frames, output_file, fps, w, h)
+    start = time.time()
+    end = time.time()
+    print(end - start)
 

5907a9bfbf9b7c39b0f6888c7a95e171fce094fd
Dina Michelson <michelsondina2011@gmail.com>
2023-05-19 00:53:25 +0300

    fixed bugs in presenting results and main window

--- Web_Client/findr-web/src/main-window/MainWindow.js
+++ Web_Client/findr-web/src/main-window/MainWindow.js
@@ -5,10 +5,49 @@ import './main-window.css';
 import axios from 'axios';
 import Logo from '../only_logo.png';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-modal';
 
 function VideoUploader() {
   const [videoFile, setVideoFile] = useState(null);
   const navigate = useNavigate();
+  const [query, setQuery] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleCloseModal = () => {
+    // Close the modal
+    setShowModal(false);
+  };
+
+  const getResults = async () => {
+    if (query.trim() === '') {
+      // Show a popup or error message to prompt the user to enter a query
+      setShowModal(true);
+      return;
+    }
+    // Create a FormData object to send the file as a binary payload
+    const formData = new FormData();
+    formData.append('file', videoFile);
+    try {
+      console.log(query);
+      const response = await axios.post('http://localhost:5002/uploadfile/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      const jumpPoints = [10, 20, 30];
+  
+      if (response.status === 200) {
+        console.log('File uploaded successfully');
+        navigate('/videoPlayer/', { state: { videoFile: videoFile, jumpPoints } });
+      }
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
 
   const handleDrop = async (acceptedFiles) => {
     const file = acceptedFiles[0];
@@ -16,28 +55,6 @@ function VideoUploader() {
     if (file.type.startsWith('video/')) {
       setVideoFile(file);
       
-      // Create a FormData object to send the file as a binary payload
-      const formData = new FormData();
-      formData.append('file', file);
-  
-      // Send the file using an Axios POST request
-      try {
-        const response = await axios.post('http://localhost:5002/uploadfile/', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        });
-        const jumpPoints = [10, 20, 30];
-        
-        if (response.status === 200){
-          console.log('File uploaded successfully');
-          navigate('/videoPlayer/', { state: { videoFile: file, jumpPoints } });
-        }
-
-
-      } catch (error) {
-        console.error('Error uploading file:', error);
-      }
     } else {
       console.log('Please upload a video file.');
     }
@@ -74,7 +91,15 @@ function VideoUploader() {
           </div>
       }
 
-      <input id='query_bar' type="text" className="form-control" placeholder="Enter your query here" />
+      <input id='query_bar' type="text" className="form-control" placeholder="Enter your query here" value={query} onChange={handleQueryChange}/>
+      <button type="button" className="btn btn-custom get-results-button" onClick={getResults}>Find results</button>
+      {showModal && (
+        <div className="popup">
+          <h2>Error</h2>
+          <p>Please enter a query.</p>
+          <button type="button" onClick={handleCloseModal}>Close</button>
+        </div>
+      )}
     </div>
   );
 }
--- Web_Client/findr-web/src/results/results.js
+++ Web_Client/findr-web/src/results/results.js
@@ -1,6 +1,7 @@
 import React, { useState,useEffect, useRef } from "react";
 import './results.css';
 import ReactPlayer from 'react-player';
+import Logo from '../only_logo.png';
 
 function VideoPlayer(props) {
     const videoRef = useRef(null);
@@ -24,24 +25,36 @@ function VideoPlayer(props) {
     };
   
     return (
-      <div>
-        <ReactPlayer
-          ref={videoRef}
-          url={URL.createObjectURL(props.video)}
-          playing={playing}
-          onSeek={handleSeek}
-          onEnded={() => setPlaying(false)}
-          controls
-        />
-        <div>
-          {props.numberList.map((number, index) => (
-            <button key={index} onClick={() => handleJumpButtonClick(number)}>
-              Jump to {number}
-            </button>
-          ))}
+        <div id='main_div'>
+          <div className="logo_header">
+            <img src={Logo} alt="Logo" className="logo" />
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '420px', marginTop: '20px' }}>
+            <div style={{ display: 'grid', placeItems: 'center', width: '80%', aspectRatio: '16/9', backgroundColor: 'black' }}>
+              <ReactPlayer
+                ref={videoRef}
+                url={URL.createObjectURL(props.video)}
+                playing={playing}
+                onSeek={handleSeek}
+                onEnded={() => setPlaying(false)}
+                controls
+                width="100%"
+                height="100%"
+              />
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px' }}>
+              {props.numberList.map((number, index) => (
+                <button key={index} onClick={() => handleJumpButtonClick(number)}>
+                  Jump to {number}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+      
+      
+      
   }
   
 export default VideoPlayer;

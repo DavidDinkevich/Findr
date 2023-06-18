1befc223a5214927a1dc1e6358a0e95deaa28f0a
David D <david.dinkevich@gmail.com>
2023-06-18 23:17:19 +0300

    Changed comment

--- Web_Client/findr-web/src/main-window/MainWindow.js
+++ Web_Client/findr-web/src/main-window/MainWindow.js
@@ -171,7 +171,7 @@ function VideoUploader() {
 
       </div>
       <div className="checkbox-section">
-        <p>Choose which algorithms we should run for you (hover above for explanations):</p>
+        <p>Choose which AI models to run (hover above for explanations):</p>
           <table>
             <tbody>
             <tr>
